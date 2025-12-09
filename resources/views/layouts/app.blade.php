7e59da6ceb91f6585f5d5675752d6cc11b67b328
Sarwar Hossain <sarwar.cse4.bu@gmail.com>
2025-12-09 23:53:12 +0600

    Update favicon to SVG format and adjust icon link in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
     
     <title>@yield('title', 'ToolHub - Free Online Tools')</title>
     <meta name="description" content="@yield('description', 'Free online tools for developers and professionals. QR code generator, URL shortener, and more useful utilities.')">
